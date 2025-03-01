05a033bb2f57547aeb355d2732c0e6a9cbf86268
Ghady Abboud <abboudghady@gmail.com>
2025-03-01 14:20:57 -0500

    Fix Broken Rendering Component

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -1,15 +1,14 @@
 import "./App.css";
 import SignIn from "./components/auth/SignIn";
 import SignUp from "./components/auth/SignUp";
+import Home from "./components/Home";
 import { Routes, Route } from "react-router";
 
 export default function App() {
   return (
     <>
       <Routes>
-        {/* 
         <Route path="/" element={<Home />} />
-        */}
         <Route path="/signup" element={<SignUp />} />
         <Route path="/signin" element={<SignIn />} />
       </Routes>

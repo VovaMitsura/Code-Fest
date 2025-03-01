bc3802cdafa3df214b7af4b86f3a8737a2555311
Ghady Abboud <abboudghady@gmail.com>
2025-03-01 13:36:49 -0500

    start.sh modification

--- start.sh
+++ start.sh
@@ -1,9 +1,23 @@
-#!bin/bash
+#!/bin/bash
+
+# Function to kill background processes when script terminates
+cleanup() {
+  echo "Stopping all processes..."
+  kill $(jobs -p) 2>/dev/null
+  exit
+}
+
+# Set up trap to catch termination signals
+trap cleanup SIGINT SIGTERM
 
 # Start backend
 cd server
 npm install
 npm run dev &
+
 cd ../react-app
 npm install
 npm run dev &
+
+echo "Services started. Press Ctrl+C to stop all processes."
+wait

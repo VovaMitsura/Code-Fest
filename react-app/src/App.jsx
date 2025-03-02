4829eac81c325187d475420e515c37440564d197
Ghady Abboud <abboudghady@gmail.com>
2025-03-01 23:32:32 -0500

    Routing Fix

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -9,7 +9,7 @@ export default function App() {
   return (
     <>
       <Routes>
-        <Route path="/" element={<Navigate to="/signin" />} />
+        <Route path="/" element={<Navigate to="/homepage" />} />
         <Route path="/homepage" element={<HomePage />} />
         <Route path="/signup" element={<SignUp />} />
         <Route path="/signin" element={<SignIn />} />

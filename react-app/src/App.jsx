2c94baab47dbeb56586c4ecc0ba2c2a6c64cc736
Ghady Abboud <abboudghady@gmail.com>
2025-03-09 13:21:34 -0400

    Removed Productivity

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -1,8 +1,7 @@
 import "./App.css";
 import SignIn from "./components/auth/SignIn";
 import SignUp from "./components/auth/SignUp";
-import HomePage from "./views/Home/homepage.jsx";
-import Productivity from "./views/Productivity/Productivity.jsx";
+import HomePage from "./views/Home/homepage";
 import { Routes, Route, Navigate } from "react-router";
 
 export default function App() {
@@ -13,7 +12,6 @@ export default function App() {
         <Route path="/homepage" element={<HomePage />} />
         <Route path="/signup" element={<SignUp />} />
         <Route path="/signin" element={<SignIn />} />
-        <Route path="/productivity" element={<Productivity />} />
       </Routes>
     </>
   );

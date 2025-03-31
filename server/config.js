8518e61177488a43a88d8ed46b9d4dbc9235c2a4
Ghady Abboud <abboudghady@gmail.com>
2025-03-31 14:04:35 -0400

    Modified config file to hide keys

--- server/config.js
+++ server/config.js
@@ -3,7 +3,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export const PORT = process.env.PORT || 5000;
-export const BLEND_AI_API_URL = "https://api.bland.ai/v1/calls";
+export const BLEND_AI_API_URL = process.env.BLEND_AI_API_URL || "";
 export const BLEND_AI_API_KEY = process.env.BLEND_AI_API_KEY || "";
-export const OPENAI_API_URL = "https://api.openai.com/v1/chat/completions";
+export const OPENAI_API_URL = process.env.OPENAI_API_URL || "";
 export const OPEN_AI_API_KEY = process.env.OPEN_AI_API_KEY || "";
